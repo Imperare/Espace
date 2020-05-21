45cb3637ce98027915ed793f80a2b39ff1b04430
Ludovic Baudin <baudin.pro@gmail.com>
2020-05-21 10:09:28 +0200

    Implémentation des astres, fusée et premiers déplacements.

---
+++ Assets/CameraCentree.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraCentree : MonoBehaviour
+{
+    public GameObject fusee;
+    public float timeOffset;
+    public Vector3 posOffset;
+    public Vector3 velocite;
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, fusee.transform.position + posOffset, ref velocite, timeOffset);
+    }
+}
