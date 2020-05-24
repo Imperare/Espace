f79fe36809efdbc4903e8923c70d537b49a86068
Ludovic Baudin <baudin.pro@gmail.com>
2020-05-24 23:27:13 +0200

    Rotation et accélération de la fusée avec les flèches directionnelles

--- Assets/Scripts/FuseeDeplacements.cs
+++ Assets/Scripts/FuseeDeplacements.cs
@@ -5,16 +5,10 @@ public class FuseeDeplacements : MonoBehaviour
     public float vitesseDeplacement;
     public Rigidbody2D rb;
     private Vector3 velocite = Vector3.zero;
-    public float RotateSpeed = 3000;
-
-    private System.Random m_random;
-    private int m_nombreDeplacements;
+    public float vitesse = 0.5f;
 
     private void Start()
     {
-        m_random = new System.Random();
-
-        m_nombreDeplacements = m_random.Next(500, 5000);
     }
 
     void OnTriggerStay(Collider col)
@@ -33,6 +27,27 @@ public class FuseeDeplacements : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.up * 0.5f * Time.deltaTime);
+        transform.Translate(Vector3.up * vitesse * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * 120 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward * -120 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vitesse += 0.03f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            vitesse -= 0.03f;
+            vitesse = System.Math.Max(0.1f, vitesse);
+        }
     }
 }
